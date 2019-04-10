7531532b4235fe93aac3a85bc3c3b2132853c182
fkgrim <fg@link2b.fr>
2019-04-10 20:39:20 +0200

    Ajouts de vidéo et flux flikr, page BD et Plac & Pluc

--- js/galleries.js
+++ js/galleries.js
@@ -127,3 +127,107 @@ jQuery('#gallery4').on('click', function (e) {
         }]
     })
 });
+
+// GALLERY Univ
+jQuery('#gallery_video_univ').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: false,
+        fullScreen: false,
+        autoplayControls: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Videos
+        dynamicEl: [
+            {
+                "src": "https://vimeo.com/167397631",
+                "subHtml": "MAMBO CANAILLE - Une expérience de livre augmenté unique"
+            },
+            {
+                "src": "https://vimeo.com/164710312",
+                "subHtml": "MAMBO CANAILLE -L'envolé du Gaspacho"
+            },
+            { // You Tube videos work only on a server.
+                "src": "https://vimeo.com/270620323",
+                "subHtml": "HOLY WOOD - Tommy: portrait fantasmé d’un auteur"
+            },
+            {
+                "src": "https://vimeo.com/300293290",
+                "subHtml": "HOLY WOOD - La Biche et le Castor"
+            },
+            {
+                "src": "https://vimeo.com/272906442",
+                "subHtml": "HOLY WOOD - Ivy For Real"
+            },
+            {
+                "src": "https://vimeo.com/272906268",
+                "subHtml": "HOLY WOOD - Norma - Si Marilyn n’avait jamais existé"
+            },
+            {
+                "src": "https://vimeo.com/218363093",
+                "subHtml": "JOSEPH APPRIN - Regards sur notre lieu de vie"
+            },
+            {
+                "src": "https://vimeo.com/218344455",
+                "subHtml": "JOSEPH APPRIN - Portraits, autoportraits"
+            },
+            {
+                "src": "https://vimeo.com/218341458",
+                "subHtml": "JOSEPH APPRIN - Conservation de la mémoire"
+            },
+            {
+                "src": "https://vimeo.com/194946965",
+                "subHtml": "L’HISTOIRE DU MAGASIN - Chapitre 1"
+            },
+            {
+                "src": "https://vimeo.com/194947388",
+                "subHtml": "L’HISTOIRE DU MAGASIN - Chapitre 2"
+            },
+            {
+                "src": "https://vimeo.com/194949013",
+                "subHtml": "L’HISTOIRE DU MAGASIN - Chapitre 3"
+            }
+        ]
+    });
+});
+
+// GALLERY 2
+jQuery('#gallery_video_placpluc').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: false,
+        fullScreen: false,
+        autoplayControls: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Videos
+        dynamicEl: [
+            { // You Tube videos work only on a server.
+                "src": "https://youtu.be/2TLJqXmzq3A",
+                "subHtml": "Plac & Pluc - E01"
+            },
+            { // You Tube videos work only on a server.
+                "src": "https://youtu.be/HYrMsRxOoS4",
+                "subHtml": "Plac & Pluc - E02"
+            },
+            { // You Tube videos work only on a server.
+                "src": "https://youtu.be/ipS26T3xheU",
+                "subHtml": "Plac & Pluc - E03"
+            },
+            { // You Tube videos work only on a server.
+                "src": "https://youtu.be/hM19NM-XxM0",
+                "subHtml": "Plac & Pluc - E04"
+            },
+        ]
+    });
+});
